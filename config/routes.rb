514aa369ba80e9bba9d4f3e4d8dcb221014abb89
Lenny <lenny.l.miller@gmail.com>
2021-07-21 18:54:10 -0700

    almost done with api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :events
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'signin', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
