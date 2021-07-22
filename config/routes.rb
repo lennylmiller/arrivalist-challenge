64191c8643b3abcc1839c755fd4a71c6bac19e85
Lenny <lenny.l.miller@gmail.com>
2021-07-21 17:38:39 -0700

    added messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'signin', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
