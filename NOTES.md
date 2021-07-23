6207003ea61ad95dd825a0456bf3b6b76f9a01e5
Lenny <lenny.l.miller@gmail.com>
2021-07-22 18:00:08 -0700

    baselined

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+Items if there is time
+* [ ] No serializer found for resource: #<Event id: 1, trip_date: "2019-12-08", home_state: "AK", trip_count: 641, created_at: "2021-07-22 00:07:26.510371000 +0000", updated_at: "2021-07-22 00:07:26.510371000 +0000">
+  [active_model_serializers] Rendered ActiveModel::Serializer::CollectionSerializer with Event::ActiveRecord_Relation (24.42ms)
+  Completed 200 OK in 44ms (Views: 31.6ms | ActiveRecord: 1.8ms | Allocations: 42690
+* [ ] 
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,10 @@ class EventsController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => :create
 
   def index
-    @events = Event.all.paginate(:page => params[:page], :per_page => params[:per_page]) #.order('created_at ASC')
-    respond_to do |format|
-      format.html
-      format.json { @projects }
+    if params[:page] and params[:per_page]
+      render json: Event.all.paginate(:page => params[:page], :per_page => params[:per_page]) #.order('created_at ASC')
+    else
+      render json: Event.all
     end
   end
 
